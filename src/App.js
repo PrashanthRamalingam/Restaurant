b1dd63bd408dce8cd0cf615135e048cf849c7e54
Prashanth Ramalingam <prashr88@gmail.com>
2022-03-31 07:20:01 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
- 
+import logo from './logo.svg';
 import './App.css';
-import Foodite from './Components/Foodite';
-import Drinks from './Components/Drinks';
+
 function App() {
   return (
-
-    <div>  
- <Foodite></Foodite>
- <Drinks></Drinks>
-</div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
