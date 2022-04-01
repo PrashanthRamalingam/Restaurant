ee2b65ca9cf8d9e5e111463d373f2fada69f1382
Prashanth Ramalingam <prashr88@gmail.com>
2022-04-01 17:20:26 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Foodite from './Components/Foodite.js';
 import Drinks from'./Components/Drinks';
 import './App.css';
+import Footer from './Components/Footer.js';
 
 function App() {
   return (
     <div  >
      <Foodite>    </Foodite>;
      <Drinks>    </Drinks>;
+      
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+ 
+ 
+import React from 'react';
+class Footer extends React.Component{
+    constructor(){
+        super();
+       this.name={ rName:'White Petals',
+        rLoc:'Bangalore',
+        
+        }}
+          render=()=>{
+
+            return(
+            <h5 style={{  image:'https://www.privacypolicies.com/public/images/logo-white.svg', color:'Blue',textAlign:'center',bottom:'-300px',position:'absolute'}}>Copyright - {this.name.rName} || {this.name.rLoc}  - PrivacyPolicies.com © 2002 - 2022 All rights reserved
+
+ 
+  </h5>
+             ) }
+
+}
+ export default Footer;
+
+ 
